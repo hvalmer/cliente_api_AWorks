db1c59264f53386d62d2e4a9fc337db0333f6972
hvalmer <hvalmer@gmail.com>
2021-05-23 20:04:52 -0300

    implantacao do CRUD

---
+++ src/main/java/com/braincustom/distlog/DistlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.braincustom.distlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/braincustom/distlog/DistlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braincustom.distlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
